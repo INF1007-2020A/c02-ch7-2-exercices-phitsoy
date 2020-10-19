d15c5bb683b116cf0810dd5069131d0f1d98e3c2
phitsoy <71102006+phitsoy@users.noreply.github.com>
2020-10-19 19:05:59 -0400

    En classe

--- exercice.py
+++ exercice.py
@@ -13,21 +13,58 @@ def build_note_dictionaries(note_names, add_octave_no=True):
 	midi_to_name = {}
 	name_to_midi = {}
 	# Pour chaque octave de 0 à 8 (inclus). On va générer tout l'octave 8, même si la dernière note du piano est Do 8
+	for octave in range(8+1):
 		# Pour chaque note de l'octave
+		for note in range(NOTES_PER_OCTAVE):
 			# Calculer le numéro MIDI de la note et ajouter aux deux dictionnaires
+			midi_no = C0_MIDI_NO + octave * NOTES_PER_OCTAVE + note
 			# Ajouter le numéro de l'octave au nom de la note si add_octave_no est vrai
+			full_note_name = note_names[note] + (str(octave) if add_octave_no else "")
+			midi_to_name[midi_no] = full_note_name
 			# Garder les numéros de notes dans name_to_midi entre 0 et 11 si add_octave_no est faux
+			name_to_midi[full_note_name] = midi_no if add_octave_no else midi_no % NOTES_PER_OCTAVE
 	return midi_to_name, name_to_midi
 
 def build_print_note_name_callback(midi_to_name):
-	pass
+	#Fonction locale
+	#Soit message MIDI midi_msg, midi_msg.type donne le type, midi_msg.note donne la note
+
+	#Dans mon callback
+	def callback(midi_msg):
+		#Si j'ai un note_on et une velocite > 0, alors j'affiche le nom de la note associee au numero (grace au dict midi_to_name
+		if midi_msg.type == "note_on" and midi_msg.velocity > 0:
+			print(midi_to_name[midi_msg.note])
+	return callback
 
 def build_print_chord_name_callback(chord_names_and_notes, name_to_midi):
 	# Construire le dictionnaire d'assocations entre état des notes et accord joué.
-	
-	# Créez et retourner le callback
-	pass
+	chords = {}
 
+	# [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0] -> "Do majeur"
+	for name, notes in chord_names_and_notes.items():
+		chord_notes = [False] * 12
+		for note in notes:
+			chord_notes[name_to_midi[note] % NOTES_PER_OCTAVE] = True
+		chords[tuple[chord_notes]] = name
+
+	# Créez et retourner le callback
+	def callback(midi_msg):
+		global note_states
+		# Si une note est appuyee
+		if midi_msg.type == "note_on" and midi_msg.velocity > 0:
+			# Je mets son element correspondant dans l'etat du clavier a True
+			note_states[midi_msg.note % NOTES_PER_OCTAVE] = True
+			note_states_tuple = tuple(note_states)
+			# Si accord connu
+			if note_states_tuple in chords:
+				# Affiche nom de l'accord
+				print(chords[note_states_tuple])
+		elif midi_msg.type == "note_off" or (midi_msg.type == "note_on" and midi_msg.velocity == 0):
+			# Idem mais à False
+			note_states[midi_msg.note % NOTES_PER_OCTAVE] = False
+	return callback
+
+note_states = [False] * 12
 
 def main():
 	PORT_MIDI = "UnPortMIDI 0"
@@ -59,6 +96,7 @@ def main():
 		"Do majeur" : ("Do", "Mi", "Sol"),
 		"Fa majeur" : ("Fa", "La", "Do"),
 		"Sol majeur" : ("Sol", "Si", "Ré"),
+		"La mineur" : ("La", "Do", "Mi")
 	}
 	
 	midi_to_name, name_to_midi = build_note_dictionaries(solfeggio_names, False)
@@ -70,3 +108,4 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
